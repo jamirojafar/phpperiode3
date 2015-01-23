092443f565578f9b1bcb44b3463ed6237ead5c40
saebuabu <a.saebu@kw1c.nl>
2015-01-23 09:15:26 +0100

    nieuwe dir en index.php

---
+++ portfolio/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Mijn portfolio site</title>
+    </head>
+    <body>
+        <h1><?php echo "portfolio site"; ?></h1>
+    </body>
+</html>
